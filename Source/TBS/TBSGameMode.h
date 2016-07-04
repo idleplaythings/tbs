59c6438fc9021da1816348a0ace25628eabd6fa9
Niko Nevala <niko@nevala.fi>
2016-07-04 16:42:50 +0300

    Add sources

---
+++ Source/TBS/TBSGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "TBSGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TBS_API ATBSGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
