47f81ffb6a40276c18b647928fccdf298ca890d6
Niko Nevala <niko@nevala.fi>
2016-07-05 09:55:42 +0300

    Add base class overrides

---
+++ Source/TBS/TBSDefaultPawn.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/DefaultPawn.h"
+#include "TBSDefaultPawn.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TBS_API ATBSDefaultPawn : public ADefaultPawn
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
---
+++ Source/TBS/TBSHud.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/HUD.h"
+#include "TBSHud.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TBS_API ATBSHud : public AHUD
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
